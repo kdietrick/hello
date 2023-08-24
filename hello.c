f4604bc3f3068c3f14c350fda44b80c564a0ff51
kdietrick <kdietrick2@huskers.unl.edu>
2023-08-24 09:12:46 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Kash Dietrick
+ * Date: 2023/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("kdietrick\n");
+
+  return 0;
+}
